74a98f4b31a9c8000af55865e6e1b95f59ae6418
Jamie Hong <jamiehong@berkeley.edu>
2024-04-28 21:04:11 -0700

    [kyria][rev3] Main layer: escape on tap or hold

--- keyboards/splitkb/kyria/keymaps/cyfr/keymap.c
+++ keyboards/splitkb/kyria/keymaps/cyfr/keymap.c
@@ -87,15 +87,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |  Fun | Nav  |  | Fun  |  Sym |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | LCtl | LAlt | LGui |  Sym | Num  |  | Nav  |Space/| Esc/ | RAlt | RCtl |
- *                        |      |      |      |      |      |  |      | Num  | Space|      |      |
+ *                        | LCtl | LAlt | LGui |  Sym | Num  |  | Nav  |Space/| Esc  | RAlt | RCtl |
+ *                        |      |      |      |      |      |  |      | Num  |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
 [_QWERTY] = LAYOUT(
     KC_TAB  , KC_Q ,  KC_W ,  KC_E ,  KC_R  ,   KC_T ,                                 KC_Y       ,   KC_U , KC_I   , KC_O   , KC_P   , KC_BSPC ,
     KC_LCTL , KC_A ,  KC_S ,  KC_D ,  KC_F  ,   KC_G ,                                 KC_H       ,   KC_J , KC_K   , KC_L   , KC_SCLN, KC_ENT  ,
     KC_LSFT , KC_Z ,  KC_X ,  KC_C ,  KC_V  ,   KC_B , FUN, NAV, FUN, SYM            , KC_N       ,   KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT ,
-                            KC_LCTL, KC_LALT, KC_LGUI, SYM, NUM, NAV, LT(_NUM,KC_SPC), TD(ESC_SPC), KC_RALT, KC_RCTL
+                            KC_LCTL, KC_LALT, KC_LGUI, SYM, NUM, NAV, LT(_NUM,KC_SPC), KC_ESC, KC_RALT, KC_RCTL
 ),
 
 /*
